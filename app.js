110327946a036284eb1c986196954c20e5764d75
cranberryJam <primakinaa@gmail.com>
2016-09-12 14:30:45 +0200

    v3

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '08aef991-c017-4ae1-a5fb-27846a6b374a',
+    appPassword: 'eCrzSae8t3Vqz86ctxzPJFP'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
