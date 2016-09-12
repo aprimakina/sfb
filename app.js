67fa76557f30927a9de39066dff4f00b5da8f6f4
cranberryJam <primakinaa@gmail.com>
2016-09-12 14:51:17 +0200

    app

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '08aef991-c017-4ae1-a5fb-27846a6b374a',
+    appPassword: 'eCrzSae8t3Vqz86ctxzPJFP'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
