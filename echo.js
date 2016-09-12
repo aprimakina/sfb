925b9f70473304167650b217df5690d2304b8bd2
cranberryJam <primakinaa@gmail.com>
2016-09-12 14:02:41 +0200

    v2

---
+++ echo.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const restify = require('restify');
+const skype = require('skype-sdk');
+
+const botService = new skype.BotService({
+    messaging: {
+        botId: '28:process.env.BOT_ID',
+        serverUrl : "https://apis.skype.com",
+        requestTimeout : 15000,
+        appId: '08aef991-c017-4ae1-a5fb-27846a6b374a',
+        appSecret: 'eCrzSae8t3Vqz86ctxzPJFP'
+    }
+});
+
+botService.on('contactAdded', (bot, data) => {
+    bot.reply(`Hello ${data.fromDisplayName}!`, true);
+});
+
+botService.on('personalMessage', (bot, data) => {
+    bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
+});
+
+const server = restify.createServer();
+
+/* Uncomment following lines to enable https verification for Azure.
+server.use(skype.ensureHttps(true));
+server.use(skype.verifySkypeCert({}));
+*/
+
+server.post('/v1/chat', skype.messagingHandler(botService));
+const port = process.env.PORT || 8080;
+server.listen(port);
+console.log('Listening for incoming requests on port ' + port);
